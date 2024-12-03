0451e23d00d0af93d5642eee8fce797b7b7e79aa
rqueiroz-suse <rhuan.queiroz@suse.com>
2024-12-03 18:34:38 -0300

    [WIP] Refactoring sanity test wrapper

--- tests/publiccloud/aistack_sanity_run.pm
+++ tests/publiccloud/aistack_sanity_run.pm
@@ -7,6 +7,10 @@ use publiccloud::utils;
 use version_utils;
 use transactional qw(process_reboot trup_install trup_shell trup_call);
 
+sub test_flags {
+    return {fatal => 1, publiccloud_multi_module => 1};
+}
+
 sub run {
     my ($self, $args) = @_;
 
@@ -30,7 +34,7 @@ sub run {
     assert_script_run("curl -O " . $sanity_tests_url);
     assert_script_run("mkdir " . $test_folder);
     assert_script_run("tar -xzvf open-webui-sanity-tests.tar.gz -C " . $test_folder);
-    zypper_call("in python311");
+    trup_call("pkg in python311");
     assert_script_run("python3 -m venv " . $test_folder . "/venv"); 
     assert_script_run("source " . $test_folder . "/venv/bin/activate");
     assert_script_run("pip3 install -r ./" . $test_folder . "/requirements.txt");
